5cd54304a9219fc6ef20a564f8e41681840ee459
Shivanu Bhateja <Shivanu.Bhateja@Honeywell.com>
2019-08-29 18:20:11 +0530

    win

--- ble-backend/app.js
+++ ble-backend/app.js
@@ -4,7 +4,12 @@ var cors = require('cors');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 require('./mongoConfig');
+const Influx = require('influx');
 var { beaconModel } = require('./models/staticBeacons');
+const influx = new Influx.InfluxDB('http://admin:admin@10.78.32.157:8086/RTLS_Engine');
+
+
+//rtlss_ble_details
 
 var app = express();
 
@@ -17,6 +22,11 @@ app.get('/', (req, res) => {
   res.send("IT WORKS!!!!");
 })
 
+app.get('/getLatestPosition', (req, res) => {
+    influx.query('select * from RTLSS_BLE_DETAILS order by time desc limit 1').then(results => {
+        res.json(results)
+      })
+})
 app.post('/addBeacon', (req, res) => {
     var data = req.body.data;
     var saveObj = new beaconModel(data);
